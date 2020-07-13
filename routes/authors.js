d8d5c1cdd497bc6fb181dbd83af52b64bd173e82
hadz333 <dinohadzic28@gmail.com>
2020-07-13 14:03:24 -0700

    finished author routes

--- routes/authors.js
+++ routes/authors.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const Author = require('../models/author')
+const Book = require('../models/book')
 
 // All Authors Route
 router.get('/', async (req, res) => {
@@ -32,13 +33,75 @@ router.post('/', async (req, res) => {
 
   try {
     const newAuthor = await author.save()
-    //res.redirect('authors/${newAuthor.id}')
-    res.redirect('authors')
+    res.redirect('authors/${newAuthor.id}')
   } catch {
     res.render('authors/new', {
       author: author,
       errorMessage: "Error creating author"
     })
+  }
+})
+
+// show specific author route
+router.get('/:id', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    const books = await Book.find({ author: author.id }).limit(6).exec()
+    res.render('authors/show', {
+      author: author,
+      booksByAuthor: books
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// edit page route
+router.get('/:id/edit', async (req, res) => {
+  try {
+    const author = await Author.findById(req.params.id)
+    res.render('authors/edit', { author: author })
+  } catch {
+    res.redirect('/authors')
+  }
+
+})
+
+// update route
+router.put('/:id', async (req, res) => {
+  let author
+
+  try {
+    author = await Author.findById(req.params.id)
+    author.name = req.body.name
+    await author.save()
+    res.redirect(`/authors/${author.id}`)
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.render('/authors/edit', {
+        author: author,
+        errorMessage: "Error updating author"
+      })
+    }
+  }
+})
+
+// delete route
+router.delete('/:id', async (req, res) => {
+  let author
+
+  try {
+    author = await Author.findById(req.params.id)
+    await author.remove()
+    res.redirect('/authors')
+  } catch {
+    if (author == null) {
+      res.redirect('/')
+    } else {
+      res.redirect(`/authors/${author.id}`)
+    }
   }
 })
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const router = express.Router()
+
+// the Book collection
 const Book = require('../models/book')
 
 router.get('/', async (req, res) => {
   let books
   try {
+    // get up to 10 of the most recently added books
     books = await Book.find().sort({ createAt: 'desc' }).limit(10).exec()
   } catch {
+    // if there's an error, don't return any books
     books = []
   }
+  // render the home page (index.ejs in /views) and pass in the books selected in query above
   res.render('index', { books: books })
 })
 
